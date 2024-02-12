66acdd1417fd4c2c7a782e70fd9d9ef855623a57
Morgane <morgane@freemiumplay.com>
2024-02-12 16:09:34 +0100

    commit for local branch test, not completed

--- src/components/Form/Formnote.jsx
+++ src/components/Form/Formnote.jsx
@@ -1,15 +1,37 @@
+import { useState } from "react";
 import "../../styles/components-style/_formNote.scss";
 
 function FormNote() {
+
+  const [data, setData] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const title = e.target.title;
+    const noteText = e.target.noteText;
+    setData(values => ({...values, [title]: noteText}))
+  }
+
   return (
     <form action="" method="get" className="form-bloc">
       <label >
         A title for your note :
-      <input type="text" name="" id="" placeholder="filter email"/>
+      <input
+        type="text"
+        name="title"
+        value={title}
+        placeholder="filter email"
+        onChange={(e) => setData(e.target.title)}
+        />
       </label>
       <label >
         A description for your note :
-      <textarea placeholder="do category to sort email topic">
+      <textarea
+        placeholder="do category to sort email topic"
+        name="text"
+        value={noteText}
+        onChange={(e) => setData(e.target.noteText)}
+      >
       </textarea>
       </label>
       <div className="button-bloc">
