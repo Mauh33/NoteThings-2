2b566cb7afc880f5fd9ece9f1c264a195a01374f
Morgane <morgane@freemiumplay.com>
2024-02-12 14:31:17 +0100

    change styles on header, page creation and bugfix pagecreation issue

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,66 @@
 import "../../styles/components-style/_header.scss";
-import {Link} from "react-router-dom";
+import { useState, useCallback, useEffect } from 'react';
+import { useStateContext } from "../../utils/DispatcherContext.jsx"
+import ArrowBack from "../../assets/arrow_back.png"
+import { Link } from "react-router-dom";
 
 export default function Header() {
 
+  const {
+    isLinkVisible,
+    setLinkVisibility,
+  } = useStateContext();
+
+
+  const toggleLinkVisibility = () => {
+    setLinkVisibility(!isLinkVisible);
+  };
+
+
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  const updateScreenWidth = useCallback(() => {
+    setScreenWidth(window.innerWidth);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("resize", updateScreenWidth);
+    return () => window.removeEventListener("resize", updateScreenWidth);
+  }, [updateScreenWidth]);
+
   return (
-    <header className='header'>
-      <h1>
-        <Link to="/list-of-notes">
-          NoteThings
+    <>
+      <header className='header'>
+        <h1>
+          <Link to='/list-of-notes'>NoteThings</Link>
+        </h1>
+        <div className='collapse-bloc'>
+        {screenWidth <= 426 ? (
+          <div className='collapse-element'>
+            <div className='arrow-bloc'>
+              <img
+                className={`${
+                  isLinkVisible ? "toggle-open" : "toggle-close"
+                }`}
+                src={ArrowBack}
+                alt=''
+                onClick={toggleLinkVisibility}
+              />
+            </div>
+            <div className={`link-bloc ${isLinkVisible ? "" : "hide"}`}>
+            <Link className='creation-link' to='/creation-of-note'>
+          Go to creation note page
         </Link>
-      </h1>
-      <Link to="/creation-of-note">Go to creation note page</Link>
-    </header>
+            </div>
+          </div>
+        ) : (
+          <Link className='creation-link' to='/creation-of-note' onClick={toggleLinkVisibility}>
+          Go to creation note page
+        </Link>
+        )}
+      </div>
+      </header>
+    </>
   );
+
 }
--- src/pages/Creationpage.jsx
+++ src/pages/Creationpage.jsx
@@ -1,74 +1,27 @@
 import React from 'react';
-import { useState, useCallback, useEffect } from 'react';
 import { useStateContext } from "../utils/DispatcherContext";
-import FormNote from "../components/Form/Formnote";
 import "../styles/pages/_creationpage.scss";
-// import "../../styles/components-style/_btn.scss";
-// import ArrowBack from "../../assets/arrow_back.png";
+import "../styles/components-style/_btn.scss";
+import FormNote from "../components/Form/Formnote";
 
 function Creationpage({title}) {
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
   const {
-    isButtonVisible,
     isFormVisible,
-    setButtonVisibility,
     setFormVisibility,
   } = useStateContext();
 
-  const toggleButtonVisibility = () => {
-    setButtonVisibility(!isButtonVisible);
-  };
 
   const toggleFormVisibility = () => {
     setFormVisibility(!isFormVisible);
   };
 
 
-  const updateScreenWidth = useCallback(() => {
-    setScreenWidth(window.innerWidth);
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener("resize", updateScreenWidth);
-    return () => window.removeEventListener("resize", updateScreenWidth);
-  }, [updateScreenWidth]);
-
-
   return (
     <>
     <section className='note-form-section'>
-        <h2>{title}</h2>
-        {isFormVisible ? <FormNote /> : " "}
-      </section>
-      <div className='collapse-bloc'>
-        {screenWidth <= 426 ? (
-          <div className='collapse-element'>
-            <div className='arrow-bloc'>
-              <img
-                className={`${
-                  isButtonVisible ? "toggle-open" : "toggle-close"
-                }`}
-                src={ArrowBack}
-                alt=''
-                onClick={toggleButtonVisibility}
-              />
-            </div>
-            <div className={`button-bloc ${isButtonVisible ? "" : "hide"}`}>
-              <button
-                type='button'
-                className='btn-note'
-                onClick={toggleFormVisibility}
-              >
-                <p>Create a note</p>
-                <div className='round-container'>
-                  <div className='plus-sign'></div>
-                </div>
-              </button>
-            </div>
-          </div>
-        ) : (
-          <div className='button-bloc'>
+      <h2>{title}</h2>
+      <div className='button-bloc'>
             <button
               type='button'
               className='btn-note'
@@ -80,10 +33,10 @@ function Creationpage({title}) {
               </div>
             </button>
           </div>
-        )}
-      </div>
-    </>
-  );
+      {isFormVisible && <FormNote />}
+    </section>
+  </>
+  )
 }
 
 export default Creationpage;
