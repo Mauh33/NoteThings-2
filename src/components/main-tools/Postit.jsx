af22b8d2ac0b00c85b0f6c7a6d3e60d6d2da4566
Morgane <morgane@freemiumplay.com>
2024-02-21 12:35:08 +0100

    merging components listPage and HomePage, delete button "create", create scss style var, delete Dispatcher provider.

--- src/components/main-tools/Postit.jsx
+++ src/components/main-tools/Postit.jsx
@@ -1,13 +1,7 @@
 import { useState } from "react";
 import "../../styles/components-style/_postIt.scss";
 
-function PostIt({
-  title,
-  description,
-  id,
-  onDelete
-}) {
-
+function PostIt({ title, description, id, onDelete }) {
   const [visibleElement, setVisibility] = useState(true);
 
   const removeElement = () => {
@@ -20,32 +14,22 @@ function PostIt({
   }
 
   return (
-
-      <div className='postIt-bloc'>
-        <div className='postIt-title-bloc'>
-          <p className='postIt-title'>Title :</p>
-          <p className='postIt-title-p'>{title}</p>
-        </div>
-        <div className='postIt-description-bloc'>
-          <p className='postIt-description'>Task description :</p>
-          <br />
-          <p className='postIt-description-p'>{description}</p>
-        </div>
-        <div className="button-bloc">
-          <button
-            type="button"
-            className="btn-note"
-            onClick={removeElement}
-          >
-            <p>Delete note</p>
-            <div className="round-container">
-              <div className="minus-sign"></div>
-            </div>
-          </button>
-        </div>
+    <div className='postIt-bloc'>
+      <div className='postIt-title-bloc'>
+        <p className='postIt-title-p'>{title}</p>
       </div>
-
-
+      <div className='postIt-description-bloc'>
+        <p className='postIt-description-p'>{description}</p>
+      </div>
+      <div className='button-bloc'>
+        <button type='button' className='btn-note' onClick={removeElement}>
+          <p>Delete note</p>
+          <div className='round-container'>
+            <div className='minus-sign'></div>
+          </div>
+        </button>
+      </div>
+    </div>
   );
 }
 
--- src/pages/Creationpage.jsx
+++ src/pages/Creationpage.jsx
@@ -1,25 +1,42 @@
-import React from 'react';
-import { useStateContext } from "../utils/DispatcherContext";
+import React, { useState } from "react";
 import "../styles/pages/_creationpage.scss";
 import "../styles/components-style/_btn.scss";
-import FormNote from "../components/Form/Formnote";
 
-function Creationpage({title}) {
+const generateUniqueKey = () => {
+  const timeStamp = new Date().getTime();
+  const min = 1000;
+  const max = 9999;
+  const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+  return `${timeStamp}-${randomNumber}`;
+};
 
-  const {
-    isFormVisible,
-    setFormVisibility,
-  } = useStateContext();
+function Creationpage({ title }) {
+  const [formData, setFormData] = useState({
+    title: "",
+    noteText: "",
+  });
 
+  const handleSubmit = e => {
+    e.preventDefault();
 
-  const toggleFormVisibility = () => {
-    setFormVisibility(!isFormVisible);
+    const noteId = generateUniqueKey();
+    const newNote = { id: noteId, ...formData };
+
+    onAdd(newNote);
+
+    setFormData({
+      title: "",
+      noteText: "",
+    });
+
+    alert(
+      "Your note has been added to your list, click on NoteThings to have a view"
+    );
   };
 
-
-  const handleAddNote = (newNote) => {
-
-    const existingNotes = JSON.parse(window.localStorage.getItem("notes")) || [];
+  const handleAddNote = newNote => {
+    const existingNotes =
+      JSON.parse(window.localStorage.getItem("notes")) || [];
     const updatedNotes = [...existingNotes, newNote];
     window.localStorage.setItem("notes", JSON.stringify(updatedNotes));
     console.log("New note added:", newNote);
@@ -27,24 +44,52 @@ function Creationpage({title}) {
 
   return (
     <>
-    <section className='note-form-section'>
-      <h2>{title}</h2>
-      <div className='button-bloc'>
-            <button
-              type='button'
-              className='btn-note'
-              onClick={toggleFormVisibility}
-            >
-              <p>Create a note</p>
+      <section className='note-form-section'>
+        <h2>{title}</h2>
+        <form
+          action=''
+          method='get'
+          className='form-bloc'
+          onSubmit={handleSubmit}
+        >
+          <label>
+            A title for your note:
+            <input
+              type='text'
+              name='title'
+              id='title'
+              value={formData.title}
+              placeholder='Enter title'
+              maxLength={30}
+              onChange={e =>
+                setFormData({ ...formData, title: e.target.value })
+              }
+            />
+          </label>
+          <label>
+            A description for your note:
+            <textarea
+              placeholder='Enter description'
+              name='text'
+              value={formData.noteText}
+              maxLength={120}
+              onChange={e =>
+                setFormData({ ...formData, noteText: e.target.value })
+              }
+            />
+          </label>
+          <div className='button-bloc'>
+            <button type='submit' className='btn-note'>
+              <p>Add a note</p>
               <div className='round-container'>
                 <div className='plus-sign'></div>
               </div>
             </button>
           </div>
-      {isFormVisible && <FormNote onAdd={handleAddNote} />}
-    </section>
-  </>
-  )
+        </form>
+      </section>
+    </>
+  );
 }
 
 export default Creationpage;
